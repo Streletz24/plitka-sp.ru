97a0d08a791598783f5c8c0a7a90b6cba725150d
Streletz24 <streletz24@gmail.com>
2026-05-21 13:16:39 +0300

    Increase header logo visual size to fill navbar height

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,31 +12,37 @@ import NotFound from "./pages/NotFound.tsx";
 import ImageLightbox from "./components/ImageLightbox.tsx";
 import { CartProvider } from "./contexts/CartContext.tsx";
 import CartDrawer from "./components/CartDrawer.tsx";
+import ScrollToHash from "./components/ScrollToHash.tsx";
+import AppErrorBoundary from "./components/AppErrorBoundary.tsx";
 
 const queryClient = new QueryClient();
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <CartProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/catalog/:slug" element={<CatalogDetail />} />
-            <Route path="/prices" element={<Prices />} />
-            <Route path="/sale" element={<Sale />} />
-            <Route path="/tiling" element={<Tiling />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <ImageLightbox />
-          <CartDrawer />
-        </BrowserRouter>
-      </CartProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <AppErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <CartProvider>
+          <Toaster />
+          <Sonner />
+          <Router>
+            <ScrollToHash />
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/catalog/:slug" element={<CatalogDetail />} />
+              <Route path="/prices" element={<Prices />} />
+              <Route path="/sale" element={<Sale />} />
+              <Route path="/tiling" element={<Tiling />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+            <ImageLightbox />
+            <CartDrawer />
+          </Router>
+        </CartProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  </AppErrorBoundary>
 );
 
 export default App;
