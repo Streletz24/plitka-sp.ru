a5b9be5b0307145c1f572753f84efc502907a1dc
Streletz24 <streletz24@gmail.com>
2026-06-04 13:33:56 +0300

    Harden order blank header spacing

--- src/config/company.ts
+++
@@ -1,6 +0,0 @@
-export const COMPANY_CONTACTS = {
-  name: "Удачная Плитка",
-  phone: "+7 (916) 133-50-56",
-  email: "plitka-sp.ru@yandex.ru",
-  address: "Московская обл., г. Сергиев Посад, ул. Фестивальная, д.6А",
-} as const;
